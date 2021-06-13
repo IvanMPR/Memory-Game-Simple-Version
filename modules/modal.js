592d5c101b5286314a9d5ae453ce1e309a6bf8e8
Ivan MPR <ivsanmas@gmail.com>
2021-06-13 10:57:32 +0200

    removed modal logic to separate module file

---
+++ modules/modal.js
@@ -0,0 +1,20 @@
+import { modalWindow, modalBackground, time, helperObject } from '../app.js';
+import { displayPastTime } from './model.js';
+export function openModal() {
+  const string = `
+  <h2>CONGRATULATIONS !</h2>
+  <p>You have finished the game in ${displayPastTime(time.textContent)},
+  with ${helperObject.counterPos + helperObject.counterNeg} attempts in total !
+  </p>
+  `;
+  modalWindow.insertAdjacentHTML('afterbegin', string);
+
+  modalBackground.classList.remove('modal-closed');
+  modalBackground.classList.add('modal-open');
+}
+
+export function closeModal() {
+  modalBackground.classList.remove('modal-open');
+  modalBackground.classList.add('modal-closed');
+  location.reload();
+}
